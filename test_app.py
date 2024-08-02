d3f1a942435ba893afd7eb7e878a3662b90c4c0d
Henrique Oliveira <henrisoft3@gmail.com>
2024-08-01 22:39:09 -0300

    update test_app

--- test_app.py
+++ test_app.py
@@ -6,3 +6,11 @@ def test_add():
     assert add(-1, 1) == 0
     assert add(-1, -1) == -2
     assert add(-5, -3) == -8
+
+    
+    assert add(3, 5) == 8
+    assert add(2, 10) == 12 
+    assert add(2, 3) == 5
+    assert add(10, 7) == 17
+
+    
